ba187a61c7aaafd312e629507a53bbd7da0eee67
Bjarte Johansen <bjo013@uib.no>
2012-05-17 14:24:55 +0200

    Added check if the themes folder is there.
    If the folder is not there it is created.

--- config/face-config.el
+++ config/face-config.el
@@ -4,6 +4,13 @@
 (blink-cursor-mode nil)
 (global-linum-mode t)
 
+(let ((themes-folder
+       (expand-file-name "themes"
+                         (file-name-directory
+                          (locate-library "color-theme")))))
+  (if (not (file-exists-p themes-folder))
+      (make-directory themes-folder)))
+
 (require 'color-theme)
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-color-theme-solarized/")
 (load "color-theme-solarized.el")
