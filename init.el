f1f25fde6dccb102238480eb245eb1514cc91e49
Bjarte Johansen <bjo013@uib.no>
2012-06-14 13:52:39 +0200

    added tramp-config file to init

--- init.el
+++ init.el
@@ -33,9 +33,14 @@
 (load "clojure-config.el")
 (load "auctex-config.el")
 (load "screen-config.el")
+(load "tramp-config.el")
 
 (setq visible-bell nil)
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(show-paren-mode t)
  '(menu-bar-mode t)
