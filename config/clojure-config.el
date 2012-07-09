87327d4ca40ef607c1f38edfac2591d34424e740
Bjarte Johansen <bjo013@uib.no>
2012-07-09 15:06:20 +0200

    removed whitespace

--- config/clojure-config.el
+++ config/clojure-config.el
@@ -1,6 +1,5 @@
 ;;removing pretty-fn that lisp-starterkit introduces
 (remove-hook 'clojure-mode-hook 'esk-pretty-fn)
 
-
 (eval-after-load 'clojure-mode-hook
   '(progn (put-clojure-indent 'update-in 'defun)))
