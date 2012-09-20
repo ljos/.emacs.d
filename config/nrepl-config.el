6363debc4d3d494657bc79cd627cdb203e397f9d
Bjarte Johansen <bjo013@uib.no>
2012-09-20 11:07:57 +0200

    Added function to connect ot nrepl locally to the now specified port for nrepl

--- config/nrepl-config.el
+++ config/nrepl-config.el
@@ -4,4 +4,8 @@
 (setq nrepl-popup-stacktraces nil)
 (setq nrepl-history-file "~/.emacs.d/history/nrepl")
 
+(defun nrepl-local ()
+  (interactive)
+  (nrepl-connect "localhost" "50001"))
+
 (provide 'nrepl-config)
