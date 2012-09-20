9f7a84d89714c577f22c6d8d893deac8bb75690a
Bjarte Johansen <bjo013@uib.no>
2012-09-20 10:53:55 +0200

    no popup stacktrace and a history file for nrepl

--- config/nrepl-config.el
+++ config/nrepl-config.el
@@ -1,4 +1,7 @@
 (require 'nrepl)
 (add-hook 'nrepl-mode-hook 'enable-paredit-mode)
 
+(setq nrepl-popup-stacktraces nil)
+(setq nrepl-history-file "~/.emacs.d/history/nrepl")
+
 (provide 'nrepl-config)
