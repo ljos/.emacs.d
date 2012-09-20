eda4afc37af01db1861fce958b2adbaeb8beec41
Bjarte Johansen <bjo013@uib.no>
2012-09-20 11:07:04 +0200

    formatting

--- config/elpa-config.el
+++ config/elpa-config.el
@@ -6,33 +6,34 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(auctex
-                      auto-complete
-                      ace-jump-mode
-                      ac-nrepl
-                      browse-kill-ring
-                      clojure-mode
-                      clojurescript-mode
-                      clojure-test-mode
-                      coffee-mode
-                      color-theme
-                      color-theme-solarized
-                      find-file-in-project
-                      gist
-                      jade-mode
-                      langtool
-                      nrepl
-                      org
-                      paredit
-                      popwin
-                      prolog
-                      slime
-                      slime-repl
-                      starter-kit
-                      starter-kit-bindings
-                      starter-kit-lisp
-                      sws-mode
-                      undo-tree)
+(defvar my-packages
+  '(auctex
+    auto-complete
+    ace-jump-mode
+    ac-nrepl
+    browse-kill-ring
+    clojure-mode
+    clojurescript-mode
+    clojure-test-mode
+    coffee-mode
+    color-theme
+    color-theme-solarized
+    find-file-in-project
+    gist
+    jade-mode
+    langtool
+    nrepl
+    org
+    paredit
+    popwin
+    prolog
+    slime
+    slime-repl
+    starter-kit
+    starter-kit-bindings
+    starter-kit-lisp
+    sws-mode
+    undo-tree)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
