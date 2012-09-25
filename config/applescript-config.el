1ed3431173fd330b39e4c642052a7bffa615102d
Bjarte Johansen <bjo013@uib.no>
2012-09-25 12:26:27 +0200

    no need for applescript mode

--- config/applescript-config.el
+++
@@ -1,6 +0,0 @@
-(provide 'applescript-config)
-
-(add-to-list 'load-path "~/.emacs.d/site-lisp/applescript-mode/")
-(autoload 'applescript-mode "applescript-mode"
-  "Major mode for editing AppleScript source." t)
-(add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode))
