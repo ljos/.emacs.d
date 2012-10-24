3686d0baf27faf3aad3a132f9cfe02042804f12f
Bjarte Johansen <bjo013@uib.no>
2012-10-24 15:22:34 +0200

    Added meaning of japanese characters as comment

--- config/pomodoro-config.el
+++ config/pomodoro-config.el
@@ -3,8 +3,8 @@
 (setq pomodoro-break-start-sound "~/Music/ding.mp3")
 (setq pomodoro-work-start-sound "~/Music/ding.mp3")
 (setq pomodoro-work-start-message "Back to work!")
-(setq pomodoro-work-cycle "労働")
-(setq pomodoro-break-cycle "休止")
+(setq pomodoro-work-cycle "労働") ;; work in japanese
+(setq pomodoro-break-cycle "休止") ;; break in japanese
 
 (global-set-key (kbd "C-c p s") 'pomodoro-start)
 (global-set-key (kbd "C-c p x") 'pomodoro-stop)
