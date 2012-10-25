14fa21688ef302ad7da2a99c52fe6de378ef470e
Bjarte Johansen <bjo013@uib.no>
2012-10-25 14:29:04 +0200

    Change long break and short break
    long break is now 20 min
    short break is 7

--- config/pomodoro-config.el
+++ config/pomodoro-config.el
@@ -5,6 +5,8 @@
 (setq pomodoro-work-start-message "Back to work!")
 (setq pomodoro-work-cycle "労働") ;; work in japanese
 (setq pomodoro-break-cycle "休止") ;; break in japanese
+(setq pomodoro-long-break-time 20)
+(setq pomodoro-break-time 7)
 
 (global-set-key (kbd "C-c p s") 'pomodoro-start)
 (global-set-key (kbd "C-c p x") 'pomodoro-stop)
