7d2761dae169531af4ca8e014154be5bcf194911
Bjarte Johansen <bjo013@uib.no>
2012-10-29 23:23:41 +0100

    switch ac-expand and ac-complete keys

--- config/autocomplete-config.el
+++ config/autocomplete-config.el
@@ -4,4 +4,9 @@
 (setq ac-auto-show-menu 0.3)
 (setq ac-use-menu-map t)
 
+(define-key ac-complete-mode-map "\r" 'ac-expand)
+(define-key ac-complete-mode-map [return] 'ac-expand)
+(define-key ac-complete-mode-map "\t" 'ac-complete)
+(define-key ac-complete-mode-map [tab] 'ac-complete)
+
 (provide 'autocomplete-config)
