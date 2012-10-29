63bd21b1d52feb9c8f08fb5a80ec1e17e78897db
Bjarte Johansen <bjo013@uib.no>
2012-10-29 23:43:01 +0100

    change paredit back from autoloads
    because of starterkit-lisp paredit needs to be required and
    keys need to be reset.

--- config/paredit-config.el
+++ config/paredit-config.el
@@ -1,25 +1,23 @@
-(require 'paredit-autoloads)
-(autoload 'enable-paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing parens." t)
+(require 'paredit)
 
-(eval-after-load "paredit"
-  '(progn
-     ;;Need to fix some bad keybindings in lisp-starterkit
-     (define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-round)
-     (define-key paredit-mode-map (kbd "M-)") 'paredit-close-round-and-newline)
-     (define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
-     (define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)
-     (define-key paredit-mode-map (kbd "M-}") 'paredit-close-curly-and-newline)
-     (defun paredit-singlequote (&optional n)
-       (interactive "P")
-       (cond ((paredit-in-string-p)
-              (if (eq (cdr (paredit-string-start+end-points))
-                      (point))
-                  (forward-char)             ; We're on the closing quote.
-                (insert ?\\ ?\' )))
-             ((paredit-in-comment-p)
-              (insert ?\' ))
-             ((not (paredit-in-char-p))
-              (paredit-insert-pair n ?\' ?\' 'paredit-forward-for-quote))))))
+
+(define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-round)
+(define-key paredit-mode-map (kbd "M-)") 'paredit-close-round-and-newline)
+(define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
+(define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)
+(define-key paredit-mode-map (kbd "M-}") 'paredit-close-curly-and-newline)
+
+;;for modes that have strings in singlequotes.
+(defun paredit-singlequote (&optional n)
+  (interactive "P")
+  (cond ((paredit-in-string-p)
+         (if (eq (cdr (paredit-string-start+end-points))
+                 (point))
+             (forward-char)             ; We're on the closing quote.
+           (insert ?\\ ?\' )))
+        ((paredit-in-comment-p)
+         (insert ?\' ))
+        ((not (paredit-in-char-p))
+         (paredit-insert-pair n ?\' ?\' 'paredit-forward-for-quote))))
 
 (provide 'paredit-config)
