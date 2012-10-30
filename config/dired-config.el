4707187c04156c34ca4466929dd16acc365a0199
Bjarte Johansen <bjo013@uib.no>
2012-10-30 11:57:08 +0100

    for some reason it is now called joc-dired-single-*

--- config/dired-config.el
+++ config/dired-config.el
@@ -1,16 +1,20 @@
-(autoload 'dired-single-buffer "dired-single" "" t)
-(autoload 'dired-single-buffer-mouse "dired-single" "" t)
-(autoload 'dired-single-magic-buffer "dired-single" "" t)
-(autoload 'dired-single-toggle-buffer-name "dired-single" "" t)
+(autoload 'joc-dired-single-buffer "dired-single" "" t)
+(autoload 'joc-dired-single-buffer-mouse "dired-single" "" t)
+(autoload 'joc-dired-single-magic-buffer "dired-single" "" t)
+(autoload 'joc-dired-single-toggle-buffer-name "dired-single" "" t)
 
 (if (boundp 'dired-mode-map)
     (my-dired-init)
   (add-hook 'dired-load-hook
             ( lambda ()
-              (define-key dired-mode-map [return] 'dired-single-buffer)
-              (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+              (define-key dired-mode-map [return]
+                'joc-dired-single-buffer)
+              (define-key dired-mode-map [mouse-1]
+                'joc-dired-single-buffer-mouse)
               (define-key dired-mode-map "^"
                 (function
-                 (lambda nil (interactive) (dired-single-buffer "..")))))))
+                 (lambda ()
+                   (interactive)
+                   (joc-dired-single-buffer "..")))))))
 
 (provide 'dired-config)
