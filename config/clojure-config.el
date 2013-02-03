59ff04220376d946809d21336aac2e2b9001a0dc
Bjarte Johansen <bjo013@uib.no>
2013-02-03 14:55:56 +0100

    add advice to cljdoc to keep it within 1 line in the minibuffer

--- config/clojure-config.el
+++ config/clojure-config.el
@@ -8,11 +8,20 @@
      (put-clojure-indent 'swap! 'defun)
      (require 'clojure-jump-to-file)
      (require 'cljdoc)
+     (ad-activate 'cljdoc-get-docstring)))
 
 
 (add-hook 'clojure-mode-hook
           '(lambda () (setq ac-sources
                        (cons ac-source-yasnippet
                              ac-sources))))
+
+(defadvice cljdoc-get-docstring (after truncate-docstring)
+  (setq ad-return-value
+        (truncate-string-to-width (concat " " ad-return-value)
+                                  (- (frame-width) 10)
+                                  nil
+                                  nil
+                                  't)))
 
 (provide 'clojure-config)
