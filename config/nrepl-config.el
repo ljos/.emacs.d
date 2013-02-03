9b728d0043d41648f83144fbf3635efe431ed215
Bjarte Johansen <bjo013@uib.no>
2013-02-03 23:52:03 +0100

    It should be nrepl-mode-hook, not clojure-nrepl or just nrepl-mode

--- config/nrepl-config.el
+++ config/nrepl-config.el
@@ -4,8 +4,7 @@
 (autoload 'nrepl-connect "nrepl"
   "nrepl mode" t nil)
 
-(add-hook 'nrepl-mode 'ac-nrepl-setup)
-(add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
