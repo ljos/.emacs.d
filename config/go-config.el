2d94ec69c5d1928090f8142088eadebcf1176485
Bjarte Johansen <bjo013@uib.no>
2013-02-08 15:03:03 +0100

    add paredit to go-mode

--- config/go-config.el
+++ config/go-config.el
@@ -1,5 +1,10 @@
 (require 'go-mode-autoloads)
 
 
+(eval-after-load "go-mode"
+  '(progn
+     (define-key go-mode-map "{" 'paredit-open-curly)
+     (define-key go-mode-map "}" 'paredit-close-curly-and-newline)
+     (add-hook 'go-mode-hook 'esk-paredit-nonlisp)))
 
 (provide 'go-config)
