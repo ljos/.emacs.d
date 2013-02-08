18e2b7e164a2ce60dca8f141a64a31d4919a1f3c
Bjarte Johansen <bjo013@uib.no>
2013-02-08 15:07:16 +0100

    remove toc from latex in articles

--- config/org-config.el
+++ config/org-config.el
@@ -81,4 +81,10 @@
                                                  "/done.org"))
          "* %?\nCLOCK: %^U--%U")))
 
+(defun org-export-latex-no-toc (depth)  
+    (when depth
+      (format "%% Org-mode is exporting headings to %s levels.\n"
+              depth)))
+(setq org-export-latex-format-toc-function 'org-export-latex-no-toc)
+
 (provide 'org-config)
