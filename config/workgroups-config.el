beed41e91967786a6c7881eafbb18b4f8ffc27ec
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-04-11 13:58:45 +0200

    add missing function

--- config/workgroups-config.el
+++ config/workgroups-config.el
@@ -5,6 +5,14 @@
 
 (wg-load "~/.emacs.d/workgroups")
 
+;; Taken from http://mcpantz.org/blog/2012/10/21/moving-from-escreen-to-workgroups/
+(defun wg-save-default ()
+  "Run `wg-save' on `wg-file'."
+  (interactive)
+  (when wg-list
+    (with-temp-message ""
+      (wg-save wg-file))))
+
 (add-hook 'auto-save-hook 'wg-save-default)
 (add-hook 'kill-emacs-hook 'wg-save-default)
 
