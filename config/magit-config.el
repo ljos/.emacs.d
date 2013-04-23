232d3009eadf74600e455a133496e714a2a3fd81
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-04-23 15:05:15 +0200

    we only need to eval after load

--- config/magit-config.el
+++ config/magit-config.el
@@ -1,6 +1,7 @@
 (require 'magit-autoloads)
 (require 'magithub-autoloads)
 
-(define-key magit-log-edit-mode-map (kbd "C-x C-s") 'magit-log-edit-commit)
+(eval-after-load 'magit-log-edit-mode
+  '(define-key magit-log-edit-mode-map (kbd "C-x C-s") 'magit-log-edit-commit))
 
 (provide 'magit-config)
