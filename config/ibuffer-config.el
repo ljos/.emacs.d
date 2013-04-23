a039f550fb043a63b79c54fd79969835ac4745be
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-04-23 15:00:40 +0200

    make ibuffer sort by major mode when not sorting by vc

--- config/ibuffer-config.el
+++ config/ibuffer-config.el
@@ -1,29 +1,15 @@
 (require 'ibuffer)
 (require 'ibuffer-vc)
 
-(setq ibuffer-saved-filter-groups
-  (quote (("Default"
-           ("Org" ;; all org-related buffers
-            (mode . org-mode))
-           ("Programming"
-            (or
-             (mode . c-mode)
-             (mode . clojure-mode)
-             (mode . emacs-lisp-mode)
-             (mode . go-mode)
-             (mode . perl-mode)
-             (mode . python-mode)
-             ))))))
-
 (add-hook 'ibuffer-hook
           (lambda ()
             (ibuffer-vc-set-filter-groups-by-vc-root)
-            (unless (eq ibuffer-sorting-mode 'alphabetic)
-              (ibuffer-do-sort-by-alphabetic))))
+            (unless (eq ibuffer-sorting-mode 'major-mode)
+              (ibuffer-do-sort-by-major-mode))))
 
 (setq ibuffer-formats
       '((mark modified read-only vc-status-mini " "
-              (name 18 18 :left :elide)
+              (name 25 25 :left :elide)
               " "
               (size 9 -1 :right)
               " "
