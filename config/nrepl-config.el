979059a34493deebb82c353c4384c7a915acfa53
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-06-04 13:40:21 +0200

    add debugging tools to clojure mode

--- config/nrepl-config.el
+++ config/nrepl-config.el
@@ -4,21 +4,33 @@
 (autoload 'nrepl-connect "nrepl"
   "nrepl mode" t nil)
 
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
 
-(add-hook 'nrepl-mode-hook 'enable-paredit-mode)
-(add-hook 'nrepl-interaction-mode '(lambda () (require 'nrepl-ritz)))
-
-(eval-after-load 'nrepl
+(eval-after-load "nrepl"
   '(progn
+     (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+     (add-hook 'nrepl-mode-hook 'enable-paredit-mode)
+     (add-hook 'nrepl-mode-hook 'subword-mode)
+     (setq nrepl-hide-special-buffers t)
+     (setq nrepl-popup-stacktraces-in-repl t)
+     (setq nrepl-history-file "~/.emacs.d/history/nrepl")
      (define-key nrepl-interaction-mode-map (kbd "C-c C-n") 'nrepl-set-ns)
-     (setq nrepl-history-file "~/.emacs.d/history/nrepl")))
+     (load-file "~/.emacs.d/site-lisp/javert/nrepl-inspect.el")
+     (define-key nrepl-mode-map (kbd "C-c i") 'nrepl-inspect)
+     ))
 
 (defun nrepl-local ()
   (interactive)
   (nrepl-connect "localhost" "50001"))
+
+(require 'nrepl-ritz-autoloads)
+
+(eval-after-load "nrepl-ritz"
+  '(progn
+     (define-key nrepl-interaction-mode-map (kbd "C-c C-j" 'nrepl-javadoc))
+     (define-key nrepl-mode-map (kbd "C-c C-j" 'nrepl-javadoc))
+     (define-key nrepl-interaction-mode-map (kbd "C-c C-a" 'nrepl-apropos))
+     (define-key nrepl-mode-map (kbd "C-c C-a" 'nrepl-apropos))))
 
 (provide 'nrepl-config)
