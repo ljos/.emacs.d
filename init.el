52c3d2c81c297f31ca992d0f0378d6502114c35a
ljos <Bjarte.Johansen@gmail.com>
2013-06-25 11:29:25 +0200

    remove menu-bar on linux as it becomes a part of the window

--- init.el
+++ init.el
@@ -107,6 +107,10 @@
 (fringe-mode 0)
 ;(set-display-table-slot standard-display-table 'wrap ?↩)
 
+(if (equal system-type 'gnu/linux)
+    (menu-bar-mode -1)
+  (menu-bar-mode +1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -116,7 +120,6 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
- '(menu-bar-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
