3ce0fb8312481e49bd418275ba5a5246fc5ec0da
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-07-25 10:40:21 +0200

    fix view program in latex

--- config/auctex-config.el
+++ config/auctex-config.el
@@ -10,4 +10,8 @@
 (setq reftex-plug-into-AUCTeX t)
 (setq TeX-PDF-mode t)
 
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+             (setq TeX-view-program-list '(("Shell Default" "open %o")))
+             (setq TeX-view-program-selection '((output-pdf "Shell Default")))))
 (provide 'auctex-config)
