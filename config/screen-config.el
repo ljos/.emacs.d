964dc2e46389ee88db1c5f73250350f1732e8801
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-07-29 15:45:06 +0200

    move frame to top left corner

--- config/screen-config.el
+++ config/screen-config.el
@@ -3,22 +3,25 @@
   (interactive)
   (if window-system
   (progn
-    (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 100))
-           (add-to-list 'default-frame-alist (cons 'width 80)))
+    (add-to-list 'default-frame-alist
+                 (cons 'width
+                       (if (> (x-display-pixel-width) 1280)
+                           100 80)))
     (add-to-list 'default-frame-alist
          (cons 'height (/ (- (x-display-pixel-height) 50)
                           (frame-char-height)))))))
 
-(when (eq 'x window-system)
-  (defun x-maximize-frame ()
+(defun x-maximize-frame ()
     (interactive)
     (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                            '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
     (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                            '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
-  (x-maximize-frame))
 
-(set-frame-size-according-to-resolution)
+(if (eq 'x window-system)
+    (x-maximize-frame)
+  (set-frame-size-according-to-resolution))
+
+(set-frame-position (next-frame) 0 0)
 
 (provide 'screen-config)
