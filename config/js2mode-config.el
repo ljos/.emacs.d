a05e5b38d27eae745924f07fe876cc0b7b99e63f
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-16 20:29:49 +0200

    js2mode configs

--- config/js2mode-config.el
+++ config/js2mode-config.el
@@ -2,19 +2,14 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (eval-after-load "js2-mode"
-  (progn
-    ;; Use lambda for anonymous functions
-    (font-lock-add-keywords
-     'js2-mode `(("\\(function\\) *("
-                  (0 (progn (compose-region (match-beginning 1)
-                                            (match-end 1) "\u0192")
-                            nil)))))
+  '(progn
+     (setq-default js2-global-externs '("_"))
+     (add-hook 'js2-mode-hook
+               '(lambda () (setq ac-sources
+                                 (cons ac-source-yasnippet
+                                       ac-sources))))
+     (add-hook 'js2-mode-hook 'turn-on-smartparens-mode)))
 
-    ;; Use right arrow for return in one-line functions
-    (font-lock-add-keywords
-     'js2-mode `(("function *([^)]*) *{ *\\(return\\) "
-                  (0 (progn (compose-region (match-beginning 1)
-                                            (match-end 1) "\u2190")
-                            nil)))))))
+
 
 (provide 'js2mode-config)
