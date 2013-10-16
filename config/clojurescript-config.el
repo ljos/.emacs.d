6fd302aaec17c78713d7e7ba64454a54d07d22e4
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-16 20:28:44 +0200

    clojure-mode for clojurescript
    Just want a mode for just clojurescript, though it is completely the
    same as clojure-mode

--- config/clojurescript-config.el
+++ config/clojurescript-config.el
@@ -1,3 +1,8 @@
-(require 'clojurescript-mode-autoloads)
+(require 'clojure-mode-autoloads)
+
+(define-derived-mode clojurescript-mode clojure-mode "ClojureScript"
+  "Major mode for ClojureScript")
+
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojurescript-mode))
 
 (provide 'clojurescript-config)
