ee1871b6c16fff780cf7b1e0aab2f7496b104b45
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-16 20:30:43 +0200

    remove magithub and the C-x C-s keybinding

--- config/magit-config.el
+++ config/magit-config.el
@@ -1,7 +1,3 @@
 (require 'magit-autoloads)
-(require 'magithub-autoloads)
-
-(eval-after-load 'magit-log-edit-mode
-  '(define-key magit-log-edit-mode-map (kbd "C-x C-s") 'magit-log-edit-commit))
 
 (provide 'magit-config)
