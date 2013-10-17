98661c4db1efcdd629c9d88d12c01a8fe7fa2d7c
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-17 09:50:14 +0200

    remove the logging. Will add back when I have time to fix it

--- config/pomodoro-config.el
+++ config/pomodoro-config.el
@@ -1,31 +1,31 @@
 (require 'pomodoro-autoloads)
 
-(defvar pomodoro-time-spent (make-hash-table :test 'equal))
+;; (defvar pomodoro-time-spent (make-hash-table :test 'equal))
 
-(defmacro frontmost-p ()
-  (cond ((eq 'darwin system-type)
-         `(string= "true\n"
-                   (shell-command-to-string
-                    "osascript -e \"tell application \\\"Emacs\\\" to get frontmost\"")))
-        ((eq 'x window-system)
-         (let* ((active-window (x-window-property
-                                "_NET_ACTIVE_WINDOW" nil "WINDOW" 0 nil t))
-                (active-window-id (if (numberp active-window)
-                                      active-window
-                                    (string-to-number
-                                     (format "%x00%x"
-                                             (car active-window)
-                                             (cdr active-window)) 16)))
-                (emacs-window-id (string-to-number
-                                  (frame-parameter nil 'outer-window-id))))
-           (= emacs-window-id active-window-id)))))
+;; (defmacro frontmost-p ()
+;;   (cond ((eq 'darwin system-type)
+;;          `(string= "true\n"
+;;                    (shell-command-to-string
+;;                     "osascript -e \"tell application \\\"Emacs\\\" to get frontmost\"")))
+;;         ((eq 'x window-system)
+;;          `(let* ((active-window (x-window-property
+;;                                  "_NET_ACTIVE_WINDOW" nil "WINDOW" 0 nil t))
+;;                  (active-window-id (if (numberp active-window)
+;;                                        active-window
+;;                                      (string-to-number
+;;                                       (format "%x00%x"
+;;                                               (car active-window)
+;;                                               (cdr active-window)) 16)))
+;;                  (emacs-window-id (string-to-number
+;;                                    (frame-parameter nil 'outer-window-id))))
+;;             (= emacs-window-id active-window-id)))))
 
-(defadvice pomodoro-tick (after pomodoro-check-time-spent)
-  (if (string= pomodoro-work-cycle pomodoro-current-cycle)
-      (if (frontmost-p)
-          (let ((k (or buffer-file-truename (buffer-name))))
-            (puthash k (1+ (gethash k pomodoro-time-spent 0)) pomodoro-time-spent))
-        (puthash "nil" (1+ (gethash "nil" pomodoro-time-spent 0)) pomodoro-time-spent))))
+;; (defadvice pomodoro-tick (after pomodoro-check-time-spent)
+;;   (if (string= pomodoro-work-cycle pomodoro-current-cycle)
+;;       (if (frontmost-p)
+;;           (let ((k (or buffer-file-truename (buffer-name))))
+;;             (puthash k (1+ (gethash k pomodoro-time-spent 0)) pomodoro-time-spent))
+;;         (puthash "nil" (1+ (gethash "nil" pomodoro-time-spent 0)) pomodoro-time-spent))))
 
 ;; we need to set this out here as it is used in the modeline
 (setq pomodoro-mode-line-string "")
