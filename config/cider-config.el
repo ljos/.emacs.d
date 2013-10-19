acbb52a5c2f083e60f984a7ca0b9587101abd060
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-19 23:35:10 +0200

    remove old configs in cider

--- config/cider-config.el
+++ config/cider-config.el
@@ -1,28 +1,11 @@
-(require 'cider-autoloads)
+(require 'cider)
 
-(autoload 'nrepl-connect "nrepl"
-  "nrepl mode" t nil)
 
-;; (eval-after-load "auto-complete"
-;;   '(add-to-list 'ac-modes 'nrepl-mode))
-
-(eval-after-load "nrepl"
+(eval-after-load "cider"
   '(progn
      (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
      (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
      (add-hook 'cider-repl-mode-hook 'subword-mode)
-     (setq nrepl-hide-special-buffers t)
-     (setq nrepl-popup-stacktraces-in-repl t)
-     (setq nrepl-history-file "~/.emacs.d/history/nrepl")
-     (define-key nrepl-interaction-mode-map (kbd "C-c C-n") 'nrepl-set-ns)))
-
-;; (require 'nrepl-ritz-autoloads)
-
-;; (eval-after-load "nrepl-ritz"
-;;   '(progn
-;;      (define-key cider-interaction-mode-map (kbd "C-c C-j" 'nrepl-javadoc))
-;;      (define-key cider-repl-mode-map (kbd "C-c C-j" 'nrepl-javadoc))
-;;      (define-key cider-interaction-mode-map (kbd "C-c C-a" 'nrepl-apropos))
-;;      (define-key cider-repl-mode-map (kbd "C-c C-a" 'nrepl-apropos))))
+     (setq cider-history-file "~/.emacs.d/history/nrepl")))
 
 (provide 'cider-config)
