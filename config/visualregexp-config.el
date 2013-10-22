58f3a4006f9fb1dd270a175bb1a65f37bcb0a3b4
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-22 11:32:14 +0200

    wrong filename

--- config/visualregexp-config.el
+++ config/visualregexp-config.el
@@ -1,6 +1,6 @@
 (require 'visual-regexp-autoloads)
 
 (global-set-key (kbd "C-c r") 'vr/replace)
-(global-set-key (kbd "C-c q") 'cr/query-replace)
+(global-set-key (kbd "C-c q") 'vr/query-replace)
 
 (provide 'visualregexp-config)
