58f8c5f54262f426e2026a2213408171a3f063d8
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-10-31 12:40:58 +0100

    the name changed

--- config/cider-config.el
+++ config/cider-config.el
@@ -6,6 +6,6 @@
      (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
      (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
      (add-hook 'cider-repl-mode-hook 'subword-mode)
-     (setq cider-history-file "~/.emacs.d/history/nrepl")))
+     (setq cider-repl-history-file "~/.emacs.d/history/nrepl")))
 
 (provide 'cider-config)
