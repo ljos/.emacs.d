4d00c5ea6742f637bc89a67cf3e8bbb2b6f9c647
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-12-05 12:55:47 +0100

    use use-package :ensure

--- init.el
+++ init.el
@@ -1,12 +1,25 @@
 ;;; init.el -- Initializes emacs by loading the org babel files
 (require 'package)
+(when (< emacs-major-version 24)
+    (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
-(require 'org-install)
-(require 'ob-tangle)
-;; Load the org file
-(org-babel-load-file
- (expand-file-name "configuration.org" user-emacs-directory))
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(use-package org
+  :ensure t
+  :init
+  (org-babel-load-file
+   (expand-file-name "configuration.org" user-emacs-directory)))
 
 ;;; init.el ends here
