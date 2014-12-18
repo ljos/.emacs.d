35d836ad3b759ca11fa8cb08866fbe496fc2b51c
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-12-18 13:10:10 +0100

    update custom

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(apropos-do-all t)
  '(auth-sources
    (quote
     ("~/.authinfo" "~/.authinfo.gpg" "~/.netrc" macos-keychain-internet)))
@@ -14,20 +15,97 @@
  '(custom-safe-themes
    (quote
     ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-sane-defaults)))
  '(indent-tabs-mode nil)
  '(inhibit-startup-echo-area-message "bjarte")
  '(inhibit-startup-screen t)
  '(ispell-dictionary "english")
  '(ispell-highlight-face (quote flyspell-incorrect))
  '(ispell-program-name "/usr/local/bin/aspell")
+ '(load-prefer-newer t)
  '(message-log-max 1000)
  '(ns-pop-up-frames nil)
  '(ns-use-srgb-colorspace t)
+ '(org-structure-template-alist
+   (quote
+    (("s" "#+BEGIN_SRC ?
+
+#+END_SRC" "<src lang=\"?\">
+
+</src>")
+     ("e" "#+BEGIN_EXAMPLE
+?
+#+END_EXAMPLE" "<example>
+?
+</example>")
+     ("q" "#+BEGIN_QUOTE
+?
+#+END_QUOTE" "<quote>
+?
+</quote>")
+     ("v" "#+BEGIN_VERSE
+?
+#+END_VERSE" "<verse>
+?
+</verse>")
+     ("V" "#+BEGIN_VERBATIM
+?
+#+END_VERBATIM" "<verbatim>
+?
+</verbatim>")
+     ("c" "#+BEGIN_CENTER
+?
+#+END_CENTER" "<center>
+?
+</center>")
+     ("l" "#+BEGIN_LaTeX
+?
+#+END_LaTeX" "<literal style=\"latex\">
+?
+</literal>")
+     ("L" "#+LaTeX: " "<literal style=\"latex\">?</literal>")
+     ("h" "#+BEGIN_HTML
+?
+#+END_HTML" "<literal style=\"html\">
+?
+</literal>")
+     ("H" "#+HTML: " "<literal style=\"html\">?</literal>")
+     ("a" "#+BEGIN_ABSTRACT
+?
+#+END_ABSTRACT" "")
+     ("A" "#+ASCII: " "")
+     ("i" "#+INDEX: ?" "#+INDEX: ?")
+     ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">"))))
  '(recentf-mode t)
  '(recentf-save-file "~/.emacs.d/.recentf")
+ '(require-final-newline t)
  '(safe-local-variable-values
    (quote
-    ((eval setq ess-directory
+    ((eval load-file
+           (concat
+            (projectile-project-root)
+            ".custom.el"))
+     (eval load-file ".custom.el")
+     (eval progn
+           (setq ess-directory
+                 (projectile-project-root)
+                 default-directory
+                 (projectile-project-root))
+           (advice-add
+            (quote org-babel-tangle)
+            :around
+            (function
+             (lambda
+               (f &rest args)
+               (let
+                   ((default-directory
+                      (concat
+                       (projectile-project-root)
+                       (file-name-as-directory "src"))))
+                 (apply f args))))))
+     (eval setq ess-directory
            (projectile-project-root)
            default-directory
            (projectile-project-root))
@@ -38,6 +116,7 @@
      (eval setq ljos/project-root
            (projectile-project-root))
      (ljos/project-directory eval projectile-project-root))))
+ '(save-interprogram-paste-before-kill t)
  '(save-place t nil (saveplace))
  '(save-place-file "~/.emacs.d/.places")
  '(savehist-additional-variables
@@ -49,6 +128,7 @@
  '(show-paren-style (quote parenthesis))
  '(tab-width 4)
  '(tool-bar-mode nil)
+ '(tramp-default-method "ssh")
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
  '(uniquify-min-dir-content 2))
 (custom-set-faces
